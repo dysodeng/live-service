1b91520031b38566901286014e10e32f0163ab69
栖枝 <dysodengs@gmail.com>
2020-12-01 15:40:02 +0800

    create env.sh

---
+++ env.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# 环境变量
+export domain=www.test.com
+export mysql_host=127.0.0.1
+export mysql_port=3306
+export mysql_database=test
+export mysql_user=root
+export mysql_password=123456
+export mysql_table_prefix=
+
+export redis_host=127.0.0.1
+export redis_port=6379
+export redis_database=0
+
+export oss_access_id=
+export oss_access_key=
+export oss_end_point=
+export oss_end_point_internal=
+export oss_bucket_name=
+export root_path=uploads
+export default_storage=ali_oss
+
+export sms_sender=aliyun
+export sms_sign_name=
+export sms_access_id=
+export sms_access_key=
